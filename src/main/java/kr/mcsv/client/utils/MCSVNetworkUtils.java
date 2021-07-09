11c318ab27fdd38d8e3490b7ca8852df6f2fb8ff
alex4386 <psh010209@gmail.com>
2021-07-09 12:45:03 +0900

    chore: add Network utils

--- src/main/java/kr/mcsv/client/utils/MCSVNetworkUtils.java
+++ src/main/java/kr/mcsv/client/utils/MCSVNetworkUtils.java
@@ -5,18 +5,29 @@ import me.alex4386.gachon.network.common.http.HttpRequestMethod;
 import me.alex4386.gachon.network.common.http.HttpResponse;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 public class MCSVNetworkUtils {
-    public static String getPublicIP() {
+    public static InetAddress getPublicIP() {
         try {
             HttpRequest req = new HttpRequest(HttpRequestMethod.GET, new URL("https://icanhazip.com"));
             HttpResponse res = req.getResponse();
-            return res.response;
+
+            return InetAddress.getByName(res.response);
         } catch (IOException e) {
             e.printStackTrace();
 
+            return null;
+        }
+    }
+
+    public static InetAddress getLocalIP() {
+        try {
+            return InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
             return null;
         }
     }
