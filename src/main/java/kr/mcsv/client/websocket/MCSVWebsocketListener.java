01c5b96dac89972f832e41600d5a0998d6ab0be8
Alex4386 <psh010209@gmail.com>
2022-02-01 13:44:04 +0900

    chore: try reconnection on disconnect

--- src/main/java/kr/mcsv/client/websocket/MCSVWebsocketListener.java
+++ src/main/java/kr/mcsv/client/websocket/MCSVWebsocketListener.java
@@ -14,8 +14,11 @@ public class MCSVWebsocketListener extends WebSocketAdapter {
 
     @Override
     public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) throws Exception {
+        try {
+            // Reconnect!
+            session.ws.connect();
+        } catch(Exception e) {}
+
         super.onDisconnected(websocket, serverCloseFrame, clientCloseFrame, closedByServer);
-
-
     }
 }
