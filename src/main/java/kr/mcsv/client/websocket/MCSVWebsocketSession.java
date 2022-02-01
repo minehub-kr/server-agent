739e2d4f893a8951828f892ba77a5e4fd01c6a34
Alex4386 <psh010209@gmail.com>
2022-02-01 12:56:01 +0900

    chore: make it use serverId

--- src/main/java/kr/mcsv/client/websocket/MCSVWebsocketSession.java
+++ src/main/java/kr/mcsv/client/websocket/MCSVWebsocketSession.java
@@ -4,26 +4,29 @@ import com.neovisionaries.ws.client.WebSocket;
 import com.neovisionaries.ws.client.WebSocketFactory;
 import com.stella_it.meiling.InvalidRefreshTokenException;
 import kr.mcsv.client.authorization.MCSVAuthorization;
+import kr.mcsv.client.core.MCSVCore;
 
 import java.io.IOException;
 import java.net.URI;
 
 public class MCSVWebsocketSession {
     private boolean isConnected = false;
-    private URI wsURI = URI.create("wss://api.mcsv.kr/");
 
     WebSocket ws;
 
-    private MCSVAuthorization auth;
-    public MCSVWebsocketSession(MCSVAuthorization auth) {
-        this.auth = auth;
+    private MCSVCore core;
+    public MCSVWebsocketSession(MCSVCore core) {
+        this.core = core;
     }
 
     public WebSocket connect() throws IOException, InvalidRefreshTokenException {
         WebSocketFactory factory = new WebSocketFactory();
+
+        URI wsURI = URI.create("wss://api.mcsv.kr/servers/"+this.core.server.getServerId()+"/ws");
         factory.setServerName(wsURI.getHost());
+
         WebSocket ws = factory.createSocket("wss://localhost/endpoint");
-        ws.addHeader("Authorization", "Bearer "+auth.getAccessToken());
+        ws.addHeader("Authorization", "Bearer "+this.core.authorization.getAccessToken());
         this.ws = ws;
         return this.ws;
     }
