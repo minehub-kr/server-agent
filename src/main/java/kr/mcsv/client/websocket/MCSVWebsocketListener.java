0bb624f0c259932e71af4b4b26a77b948922847d
Alex4386 <psh010209@gmail.com>
2022-02-01 13:21:12 +0900

    chore: stream-lined websocket connection flow

--- src/main/java/kr/mcsv/client/websocket/MCSVWebsocketListener.java
+++ src/main/java/kr/mcsv/client/websocket/MCSVWebsocketListener.java
@@ -1,8 +1,21 @@
 package kr.mcsv.client.websocket;
 
+import com.neovisionaries.ws.client.WebSocket;
 import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketFrame;
 
 public class MCSVWebsocketListener extends WebSocketAdapter {
+    MCSVWebsocketSession session;
+
+    public MCSVWebsocketListener(MCSVWebsocketSession session) {
+        super();
+        this.session = session;
+    }
+
+    @Override
+    public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) throws Exception {
+        super.onDisconnected(websocket, serverCloseFrame, clientCloseFrame, closedByServer);
 
 
+    }
 }
--- src/main/java/kr/mcsv/client/websocket/MCSVWebsocketSession.java
+++ src/main/java/kr/mcsv/client/websocket/MCSVWebsocketSession.java
@@ -1,32 +1,49 @@
 package kr.mcsv.client.websocket;
 
-import com.neovisionaries.ws.client.WebSocket;
-import com.neovisionaries.ws.client.WebSocketFactory;
+import com.neovisionaries.ws.client.*;
 import com.stella_it.meiling.InvalidRefreshTokenException;
 import kr.mcsv.client.authorization.MCSVAuthorization;
 import kr.mcsv.client.core.MCSVCore;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
 
 public class MCSVWebsocketSession {
     private boolean isConnected = false;
 
     WebSocket ws;
+    WebSocketAdapter adapter = null;
 
     private MCSVCore core;
     public MCSVWebsocketSession(MCSVCore core) {
         this.core = core;
     }
 
-    public WebSocket connect() throws IOException, InvalidRefreshTokenException {
-        WebSocketFactory factory = new WebSocketFactory();
+    public WebSocket connect() throws IOException, InvalidRefreshTokenException, WebSocketException {
+        if (this.isConnected()) return null;
+        WebSocket ws;
 
-        URI wsURI = URI.create("wss://api.mcsv.kr/servers/"+this.core.server.getServerId()+"/ws");
-        factory.setServerName(wsURI.getHost());
+        if (this.ws == null) {
+            WebSocketFactory factory = new WebSocketFactory();
 
-        WebSocket ws = factory.createSocket("wss://localhost/endpoint");
+            URI wsURI = URI.create("wss://api.mcsv.kr/servers/"+this.core.server.getServerId()+"/ws");
+            factory.setServerName(wsURI.getHost());
+
+            ws = factory.createSocket("wss://localhost/endpoint");
+        } else {
+            ws = this.ws.recreate();
+        }
+
+        if (this.adapter == null) {
+            this.adapter = new MCSVWebsocketListener(this);
+        }
+
         ws.addHeader("Authorization", "Bearer "+this.core.authorization.getAccessToken());
+        ws.addListener(this.adapter);
+
+        ws.connect();
         this.ws = ws;
         return this.ws;
     }
