902577a990171574cb29fbb8abef58ff6311145d
Alex4386 <psh010209@gmail.com>
2022-02-26 11:21:23 +0000

    chore: monkey patch for synchronous bukkit

--- src/main/java/kr/mcsv/client/websocket/MCSVWebsocketHandler.java
+++ src/main/java/kr/mcsv/client/websocket/MCSVWebsocketHandler.java
@@ -1,9 +1,11 @@
 package kr.mcsv.client.websocket;
 
+import kr.mcsv.client.Main;
 import kr.mcsv.client.server.MCSVServer;
 import kr.mcsv.client.websocket.command.MCSVWebsocketCommandDispatcher;
 
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
 import org.json.simple.JSONObject;
 
 public class MCSVWebsocketHandler {
@@ -32,13 +34,20 @@ public class MCSVWebsocketHandler {
             if (cmdline == null) throw new Exception("missing cmdline");
 
             MCSVWebsocketCommandDispatcher dispatcher = new MCSVWebsocketCommandDispatcher();
-            Bukkit.dispatchCommand(dispatcher, cmdline);
+            
+            // All bukkit related stuff should be run synchronously.
+            BukkitTask task = Bukkit.getScheduler().runTask(Main.plugin, () -> {
+                Bukkit.dispatchCommand(dispatcher, cmdline);
 
-            JSONObject responseData = new JSONObject();
-            responseData.put("output", dispatcher.getOutput());
+                JSONObject responseData = new JSONObject();
+                responseData.put("output", dispatcher.getOutput());
+    
+                response.put("action", action);
+                response.put("data", responseData);
+            });
 
-            response.put("action", action);
-            response.put("data", responseData);
+            // wait until synchronous bukkit task ends.
+            task.wait();
         } else {
             response.put("action", actionStr);
             response.put("error", "invalid_action");
