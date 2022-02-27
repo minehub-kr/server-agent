31f8c5be1a9c2b5325417d426e40f1a8a9289f7c
Alex4386 <psh010209@gmail.com>
2022-02-27 17:35:10 +0000

    chore: backport

--- src/main/java/kr/mcsv/client/utils/MCSVBukkitUtils.java
+++ src/main/java/kr/mcsv/client/utils/MCSVBukkitUtils.java
@@ -1,7 +1,16 @@
 package kr.mcsv.client.utils;
 
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.World.Environment;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class MCSVBukkitUtils {
@@ -20,6 +29,123 @@ public class MCSVBukkitUtils {
         json.put("isOp", player.isOp());
 
         return json;
+    }
+
+    public static JSONObject getBukkitInfoJSON() {
+        JSONObject json = new JSONObject();
+
+        json.put("server", getBukkitServerJSON());
+        json.put("plugisn", getBukkitPluginsJSON());
+
+        return json;
+    }
+
+    public static JSONArray getBukkitPluginsJSON() {
+        JSONArray json = new JSONArray();
+        Plugin[] plugins = Bukkit.getPluginManager().getPlugins();
+        
+        for (Plugin plugin:plugins) {
+            json.add(getBukkitPluginJSON(plugin));
+        }
+
+        return json;
+    }
+
+    public static JSONObject getBukkitPluginJSON(Plugin plugin) {
+        JSONObject json = new JSONObject();
+
+        PluginDescriptionFile file = plugin.getDescription();
+
+        if (file == null) {
+            json.put("name", plugin.getName());
+        } else {
+            json.put("name", file.getName());
+            json.put("fullname", file.getFullName());
+            json.put("description", file.getDescription());
+            json.put("website", file.getWebsite());
+            json.put("version", file.getVersion());
+            json.put("main", file.getMain());
+            
+            JSONArray authors = new JSONArray();
+            for (String author: file.getAuthors()) {
+                authors.add(author);
+            }
+            json.put("authors", authors);
+        }
+
+        return json;
+    }
+
+    public static JSONObject getBukkitServerJSON() {
+        JSONObject json = new JSONObject();
+        Server server = Bukkit.getServer();
+
+        json.put("name", server.getName());
+        json.put("version", server.getVersion());
+        json.put("motd", server.getMotd());
+
+        List<World> worldsRaw = server.getWorlds();
+        JSONArray worlds = new JSONArray();
+
+        for (World worldRaw: worldsRaw) {
+            worlds.add(getWorldJSON(worldRaw));   
+        }
+        json.put("worlds", worlds);
+
+
+
+        JSONObject settings = new JSONObject();
+
+        settings.put("port", server.getPort());
+        settings.put("ip", server.getIp());
+        settings.put("hardcore", server.isHardcore());
+        settings.put("viewDistance", server.getViewDistance());
+        settings.put("maxPlayers", server.getMaxPlayers());
+
+        json.put("settings", settings);
+
+        return json;
+    }
+
+    public static JSONObject getWorldJSON(World world) {
+        JSONObject json = new JSONObject();
+
+        json.put("name", world.getName());
+        json.put("seaLevel", world.getSeaLevel());
+        json.put("time", world.getTime());
+        json.put("worldType", world.getWorldType().getName());
+        json.put("storm", world.hasStorm());
+        json.put("thundering", world.isThundering());
+        json.put("autosave", world.isAutoSave());
+        json.put("spawnLoc", getLocationJSON(world.getSpawnLocation()));
+        json.put("maxHeight", world.getMaxHeight());
+        json.put("gameRules", world.getGameRules());
+        json.put("environment", getEnvironmentString(world.getEnvironment()));
+        json.put("pvp", world.getPVP());
+
+        JSONObject spawnSettings = new JSONObject();
+        spawnSettings.put("monsters", world.getAllowMonsters());
+        spawnSettings.put("animals", world.getAllowAnimals());
+
+        json.put("spawnSettings", spawnSettings);
+
+
+        return json;
+    }
+
+    public static String getEnvironmentString(Environment env) {
+        switch(env) {
+            case NORMAL:
+                return "normal";
+            case NETHER:
+                return "nether";
+            case THE_END:
+                return "the_end";
+            case CUSTOM:
+                return "custom";
+            default:
+                return null;
+        }
     }
 
     public static JSONObject getPlayerLevelJSON(Player player) {
