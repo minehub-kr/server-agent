1445bced27aef59daca0f32bd2eb06be1045366d
Baw-Appie <pp121324@gmail.com>
2024-11-26 14:17:04 +0900

    Update GitHub CI/CD

--- .github/workflow-files/change-ver.sh
+++ .github/workflow-files/change-ver.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-test -f ./pom.xml.bak && rm ./pom.xml
-test -f ./pom.xml.bak && mv ./pom.xml.bak ./pom.xml
+test -f ./build.gradle.kts.bak && rm ./build.gradle.kts
+test -f ./build.gradle.kts.bak && mv ./build.gradle.kts.bak ./build.gradle.kts
 
 echo "Target version: $1"
-cp pom.xml pom.xml.bak
+cp build.gradle.kts build.gradle.kts.bak
 version_name=$(find ~/.m2/repository/org/spigotmc/spigot-api -type d -maxdepth 1 -name "$1*" -print -quit 2> /dev/null)
 api_ver_name="${version_name/$HOME\/.m2\/repository\/org\/spigotmc\/spigot-api\//}"
 api_ver=$(echo "$api_ver_name" | cut -f1,2 -d'.')
 
 echo "Target Spigot-API version: $api_ver_name, api-version: $api_ver"
-sed -i s/1.16.5-R0.1-SNAPSHOT/$api_ver_name/g ./pom.xml
+sed -i s/1.16.5-R0.1-SNAPSHOT/$api_ver_name/g ./build.gradle.kts
 sed -i s/1.16/$api_ver/g ./src/main/resources/plugin.yml
 
 echo "Delta Patching source code to match with SDK changes on Spigot-API: $api_ver_name"
